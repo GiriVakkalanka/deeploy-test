28226393f764821d217b42f26eb37a70805c43ae
Seshagiri Vakkalanka <gvakkalanka@seshagiris-mbp.mynetworksettings.com>
2024-05-03 00:39:13 -0400

    Revert changes to cors policy

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    // origin: ['https://deeploy-test-client.vercel.app', 'http://localhost:3000'],
-    origin: '*',
+    origin: ['https://deeploy-test-client.vercel.app', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
